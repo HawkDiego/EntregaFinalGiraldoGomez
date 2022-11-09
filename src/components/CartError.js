c9ebf4f0fea062479f0bdcda9e674b173e45d4f2
HawkDiego <judgiraldogo@unal.edu.co>
2022-11-09 17:57:47 -0500

    cambio mensaje carrito vacio

--- src/components/CartError.js
+++ src/components/CartError.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Flex, Box, Button } from '@chakra-ui/react'
 import { TiHome } from 'react-icons/ti'
+import { FaHeartBroken } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { useColorMode } from '@chakra-ui/react'
 export const CartError = () => {
-  const {colorMode} = useColorMode()
+  const { colorMode } = useColorMode()
   return (
     <Flex
-      h='30vh'
       justify='center'
       align='center'
       direction='column'
       b='whitesmoke'
       color={colorMode === 'dark' ? 'whitesmoke' : '#525151'}
+      mt='20px'
     >
-      <Box fontSize={['38px', '40px', '40px', '55px']} fontWeight='600'>
-        Tu carrito se encuentra vacio 💔
+      <Box
+        fontSize={['25px', '30px', '40px', '55px']}
+        fontWeight='600'
+        display='flex'
+        gap={5}
+        alignItems='center'
+      >
+        Tu carrito se encuentra vacio <FaHeartBroken />
       </Box>
       <Box fontSize={['18px', '20px', '20px', '25px']}>
         Volvamos a buscar algo que te guste...
